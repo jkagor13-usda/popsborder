62f3916b24d11083eb1025308c9a1f6120c28854
jkagor13-rgb <Joseph.Agor@jhuapl.edu>
2025-10-03 08:52:46 -0400

    removed original python conversion of R code

--- slippage.py
+++ slippage.py
@@ -18,10 +18,53 @@ def main():
     # Load configuration and compliance table
     config = load_configuration(config_file)
     
-    # TODO: SYNTHETIC GENERATION + rbs calc + pis  
+    # TODO: SYNTHETIC GENERATION + rbs calc + pis
+
+
+
     # TODO: Contamination data
+    ####################################################################
+    ####################################################################
+    #################    CONTAMINATION MODULE  #########################
+    ####################################################################
+    ####################################################################
+
+    ### Read in Data
+    dir = r'C:\Users\agorjk1\Box\NHH15 - USDA APHIS EDISON\05 PPQ Engagement\PPQ RBS Data (Folder shared with APHIS)\APL Created Data Related Items\Synthetic_Data'
+    filename = os.path.join(dir, f'synthetic_pis_data.csv')
+    # Load in task log from a single replication of a run
+    df_pis_data = pd.read_csv(filename)
+
+    filename = os.path.join(dir, f'synthetic_rbs_calc_data.csv')
+    # Load in task log from a single replication of a run
+    df_rbs_calculator = pd.read_csv(filename)
+
+    ### Generate clarke inputs via input data
+    inputs = gen_clarke_model_inputs(df_pis_data, df_rbs_calculator)
+
+    # Run clarke model
+    res = run_clarke_bb_group_model(inputs.ty,
+                                    inputs.b,
+                                    inputs.B,
+                                    inputs.Nbar,
+                                    inputs.freq,
+                                    inputs.theta,
+                                    inputs.R,
+                                    inputs.start_val,
+                                    inputs.se)
+    print(f'   Alpha = {res["alpha"]}')
+    print(f'   Beta = {res["beta"]}')
+    print('')
+
+
 
     config[rbs_calc_file] = YYYY
+
+    ####################################################################
+    ####################################################################
+    ################    END CONTAMINATION MODULE  ######################
+    ####################################################################
+    ####################################################################
 
     compliance_table = load_compliance_lookup_csv(compliance_file)
 
