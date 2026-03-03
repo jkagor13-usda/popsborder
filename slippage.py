59f447c84b4c1473bc249410fcf207f80b3bc453
ling1 <Gary.Lin@jhuapl.edu>
2026-03-03 12:17:34 -0500

    added unique id

--- slippage.py
+++ slippage.py
@@ -43,7 +43,10 @@ def main():
     # Synthetic data generation
     num_consignments_to_simulate = 5 # Added input parameter to be the number of consignments you want simulated
     synthetic_data_generator = SyntheticConsignmentDataGenerator(input_data_file=pis_data_updated)
-    synth_data = synthetic_data_generator.generate_from_input_data(n_consignments=num_consignments_to_simulate, sampling_method="sequential")
+    synth_data = synthetic_data_generator.generate_from_input_data(
+        n_consignments=num_consignments_to_simulate, 
+        sampling_method="sequential"
+    )
     synth_out_path = data_dir / "Syntehtic_PIS_SampleQuantity.csv"
     synth_data.to_csv(synth_out_path)
 
