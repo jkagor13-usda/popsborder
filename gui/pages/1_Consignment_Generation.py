09fb1aadd86e66bbf9a0a581b83a603f53dfe694
jkagor13-rgb <Joseph.Agor@jhuapl.edu>
2026-09-16 16:37:15 -0400

    removed debug prints from Consignment Generation page

--- gui/pages/1_Consignment_Generation.py
+++ gui/pages/1_Consignment_Generation.py
@@ -163,16 +163,6 @@ def _save_rbs_to_tmp(
 
             options: SyntheticOptions = state.get("synthetic_options", SyntheticOptions())
 
-            if producer_grouping_path is not None:
-                grouping_df = pd.read_csv(producer_grouping_path)
-
-                st.write("Producer grouping path:", str(producer_grouping_path))
-                st.write("Producer grouping columns:", grouping_df.columns.tolist())
-                st.write(
-                    "Grouping file has PRODUCER_NAME:",
-                    "PRODUCER_NAME" in grouping_df.columns,
-                )
-
             synth_df = generate_synthetic_data(
                 seed_path,
                 dest_rbs,
@@ -208,8 +198,6 @@ def _save_rbs_to_tmp(
         return False, "No RBS source file found."
 
     except Exception as exc:  # pylint: disable=broad-except
-        import traceback
-        traceback.print_exc()
         return False, f"Unable to save consignment files: {exc}"
 
 
